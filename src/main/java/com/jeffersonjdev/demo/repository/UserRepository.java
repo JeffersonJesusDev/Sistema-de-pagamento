ac3e0ff566717b7197d43f4ef3b27a8e37042d17
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-09-28 23:41:32 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/jeffersonjdev/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jeffersonjdev.demo.repository;
+
+import com.jeffersonjdev.demo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByEmail(String email);
+}
