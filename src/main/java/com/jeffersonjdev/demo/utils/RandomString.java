bea6af53df7c11841b37986dcf33686e33671a53
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-09-29 00:59:49 -0300

    Criando RandomString para criptografia

---
+++ src/main/java/com/jeffersonjdev/demo/utils/RandomString.java
@@ -0,0 +1,18 @@
+package com.jeffersonjdev.demo.utils;
+
+import java.security.SecureRandom;
+
+public class RandomString {
+
+    public static final String CHARACTERS ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String generateRandomString(int length){
+        SecureRandom secureRandom = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<length; i++){
+            int index = secureRandom.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(index));
+        }
+        return sb.toString();
+    }
+}
