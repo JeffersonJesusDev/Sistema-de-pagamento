3325b5d1e4c4cd9be545c2f13be4260bb36572cc
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-09-30 12:15:29 -0300

    Adicionando método de vericação de código email

--- src/main/java/com/jeffersonjdev/demo/service/UserService.java
+++ src/main/java/com/jeffersonjdev/demo/service/UserService.java
@@ -4,9 +4,12 @@ import com.jeffersonjdev.demo.dto.UserResponse;
 import com.jeffersonjdev.demo.entity.User;
 import com.jeffersonjdev.demo.repository.UserRepository;
 import com.jeffersonjdev.demo.utils.RandomString;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class UserService {
@@ -17,7 +20,10 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public UserResponse registerUser(User user) {
+    @Autowired
+    private MailService emailservice;
+
+    public UserResponse registerUser(User user) throws MessagingException, UnsupportedEncodingException {
         if (userRepository.findByEmail(user.getEmail()) != null){
             throw new RuntimeException("Email already registered");
         } else {
@@ -30,7 +36,26 @@ public class UserService {
             User savedUser = userRepository.save(user);
 
             UserResponse userResponse = new UserResponse(savedUser.getId(), savedUser.getName(), savedUser.getEmail(), savedUser.getPassword());
+
+            emailservice.sendVerificationEmail(user);
+
+
             return userResponse;
+        }
+    }
+
+    public boolean verify(String verificationCode){
+
+        User user = userRepository.findByVerificationCode(verificationCode);
+
+        if (user == null || user.isEnabled()){
+            return false;
+        } else {
+            user.setVerificationCode(null);
+            user.setEnabled(true);
+            userRepository.save(user);
+
+            return true;
         }
     }
 }
