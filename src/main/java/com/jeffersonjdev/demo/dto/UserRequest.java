45de09dce996c46a1c80a21aa6d424de0388b886
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-09-29 21:29:08 -0300

    Adicionando validação NotNull e Notblank

--- src/main/java/com/jeffersonjdev/demo/dto/UserRequest.java
+++ src/main/java/com/jeffersonjdev/demo/dto/UserRequest.java
@@ -1,8 +1,26 @@
 package com.jeffersonjdev.demo.dto;
 
 import com.jeffersonjdev.demo.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record UserRequest(String name, String email, String password) {
+public record UserRequest(
+        @NotNull(message = "O nome não pode ser nulo.")
+        @NotBlank(message = "O nome não pode ser vazio.")
+        String name,
+
+        @NotNull(message = "O nome não pode ser nulo.")
+        @NotBlank(message = "O nome não pode ser vazio.")
+        @Email
+        String email,
+
+
+        @NotNull(message = "O nome não pode ser nulo.")
+        @NotBlank(message = "O nome não pode ser vazio.")
+                @Size(min = 8, message = "A senha deve conter no minimo 8 caracteres.")
+        String password) {
 
     public User toModel(){
         return new User(name, email, password);
