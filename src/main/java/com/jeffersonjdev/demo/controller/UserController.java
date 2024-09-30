e7a661b2d99ed2b9009dfb3cd3ee2e86a0203a64
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-09-30 12:14:49 -0300

    Adicionando método de verificação de código para o controlador

--- src/main/java/com/jeffersonjdev/demo/controller/UserController.java
+++ src/main/java/com/jeffersonjdev/demo/controller/UserController.java
@@ -5,13 +5,14 @@ import com.jeffersonjdev.demo.dto.UserRequest;
 import com.jeffersonjdev.demo.dto.UserResponse;
 import com.jeffersonjdev.demo.entity.User;
 import com.jeffersonjdev.demo.service.UserService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
 
 
 @RestController
@@ -22,9 +23,18 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponse> registerUser(@RequestBody @Valid UserRequest userRequest) {
+    public ResponseEntity<UserResponse> registerUser(@RequestBody @Valid UserRequest userRequest) throws MessagingException, UnsupportedEncodingException {
        User user = userRequest.toModel();
        UserResponse userSaved = userService.registerUser(user);
        return ResponseEntity.ok().body(userSaved);
+    }
+
+    @GetMapping("/verify")
+    public String verifyUser(@Param("code") String code){
+        if (userService.verify(code)){
+            return "Verify_sucess";
+        } else {
+            return "verify_fail";
+        }
     }
 }
